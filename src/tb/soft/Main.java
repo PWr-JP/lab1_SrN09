703b417080bedb419299ad51b772ebbb0bce6d20
Tomasz Babczyński <tomasz.babczynski@pwr.edu.pl>
2021-10-20 10:40:59 +0200

    wypisywanie i zmienne

--- src/tb/soft/Main.java
+++ src/tb/soft/Main.java
@@ -3,8 +3,12 @@ package tb.soft;
 public class Main {
 
     public static void main(String[] args) {
+        String witaj, imie;
 
-        System.out.println("Witaj");
+        witaj="Witaj";
+        imie=args[0];
+
+        System.out.printf("%s posiadaczu imienia %10s",witaj, imie);
 
         System.exit(0);
     }
